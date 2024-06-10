04189195921294255ebb108527323eb1053136c4
juanfcarrillo <juanfi444@outlook.com>
2024-06-09 22:49:36 -0500

    feat: made first sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,18 @@
+import plugin from 'tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				'color-primary': '#0b3d91',
-				'color-primary-darker': '#0b3d91',
-				'color-primary-darkest': '#061f4a',
-				'color-base': '#212121',
-				'color-gray-dark': '#323a45',
-				'color-gray-light': '#aeb0b5',
-				'color-white': '#ffffff',
+				'ns-primary': '#105bd8',
+				'ns-primary-darker': '#0b3d91',
+				'ns-primary-darkest': '#061f4a',
+				'ns-base': '#212121',
+				'ns-gray-dark': '#323a45',
+				'ns-gray-light': '#aeb0b5',
+				'ns-white': '#ffffff',
 			}
 		},
 	},
