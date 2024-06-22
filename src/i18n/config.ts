2242ee7da93ee8b5b3ec3f2c6b2c48b16d2ee00c
Max Carrión <141116497+MaxCar31@users.noreply.github.com>
2024-06-22 12:41:07 -0500

    chore documentation of utils.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,12 @@
-// src/i18n/config.ts
+
+
 import { ui, defaultLang } from './ui';
+
+/**
+ * Función para obtener el idioma desde la URL.
+ * @param url - La URL desde la cual se extrae el idioma.
+ * @returns El idioma si está definido en 'ui', de lo contrario retorna 'defaultLang'.
+ */
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,10 +1,21 @@
 import { ui, defaultLang } from './ui';
 
+/**
+ * Función para obtener el idioma desde la URL.
+ * @param url - La URL desde la cual se extrae el idioma.
+ * @returns El idioma si está definido en 'ui', de lo contrario, retorna 'defaultLang'.
+ */
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
+
+/**
+ * Función para usar traducciones basadas en el idioma.
+ * @param lang - El idioma a utilizar para las traducciones.
+ * @returns Una función que toma una clave de traducción y retorna el texto traducido.
+ */
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
@@ -12,6 +23,11 @@ export function useTranslations(lang: keyof typeof ui) {
   }
 }
 
+/**
+ * Función para obtener una ruta traducida basada en el idioma.
+ * @param lang - El idioma a utilizar para traducir la ruta.
+ * @returns Una función que toma una ruta y, opcionalmente, un idioma, y retorna la ruta traducida.
+ */
 export function useTranslatedPath(lang: keyof typeof ui) {
   return function translatePath(path: string, l: string = lang) {
     return `/${l}${path}`;
