9946463d76deacbf1cae00fa6a1871778aefa174
david-fedora <davot098@gmail.com>
2024-06-23 18:14:38 -0500

    Init cms using Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 import tailwind from "@astrojs/tailwind";
 
@@ -11,7 +14,19 @@ export default defineConfig({
       enabled: true
     }
   }),
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+      },
+      // apiOptions: {
+      //   // Choose your Storyblok space region
+      //   region: 'us', // optional,  or 'eu' (default)
+      // },
+    })
+  ],
   redirects: {
     '/': '/colegio'
   }
