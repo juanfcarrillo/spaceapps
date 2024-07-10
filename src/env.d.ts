663b9fc31d3110a1da8d340ff97c232b2ff5687b
David Alexander Trujillo Montenegro <102611104+DavidTrujillo123@users.noreply.github.com>
2024-07-10 16:27:36 -0500

    Fix/cms (#29)
    * Fix cms: Added storyblok environments (draft/published)
    
    * Refactor storyblok environments. Add PUBLIC_MODE_DEV variable
    
    * feat(middleware): added isDraft context
    
    * feat(pages): added isDraft conditional
    
    * refactor (environment): Added middleware function to get page data
    
    * bad: added log code
    
    * bad: add loging url
    
    * fix (middleware): add new URL handler
    
    * fix: removed bad code
    
    ---------
    
    Co-authored-by: juanfcarrillo <juanfi444@outlook.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,5 +4,7 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
 	interface Locals extends Runtime {
 		STORYBLOK_TOKEN: string
+		isDraft: boolean
+		getPageContent: (page: string) => Promise<any>
 	}
 }
