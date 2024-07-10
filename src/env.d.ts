e7cb5da61ee18762cd26171e2f3bd6f26bf1a607
juanfcarrillo <juanfi444@outlook.com>
2024-07-10 15:28:10 -0500

    feat(middleware): added isDraft context

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,5 +4,6 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
 	interface Locals extends Runtime {
 		STORYBLOK_TOKEN: string
+		isDraft: boolean
 	}
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,20 @@ import { defineMiddleware } from "astro:middleware";
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
     context.locals.STORYBLOK_TOKEN = import.meta.env.STORYBLOK_TOKEN;
+    const url = import.meta.env.PUBLIC_VERCEL_URL ? new URL(import.meta.env.PUBLIC_VERCEL_URL || "") : context.url;
+    const { hostname } = url;
+
+    const subdomain = hostname.split('.')[0]
+
+    if (hostname.includes("localhost") || hostname.includes("127.0.0.1")) {
+        context.locals.isDraft = true;
+    }
+
+    if (subdomain.includes("draft")) {
+        context.locals.isDraft = true;
+    }
+
+    context.locals.isDraft = false;
+
     return next();
 });
